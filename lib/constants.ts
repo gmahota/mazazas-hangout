dc229541f9460dc689f5f488bf62081944d98814
Guimarães Mahota <guimaraesmahota@gmail.com>
2022-01-05 12:19:01 +0200

    mazazas titulo

--- lib/constants.ts
+++ lib/constants.ts
@@ -17,16 +17,16 @@
 export const SITE_URL = 'https://demo.vercel.events';
 export const SITE_ORIGIN = process.env.NEXT_PUBLIC_SITE_ORIGIN || new URL(SITE_URL).origin;
 export const TWITTER_USER_NAME = 'vercel';
-export const BRAND_NAME = 'ACME';
+export const BRAND_NAME = 'Mazazas Hangout';
 export const SITE_NAME_MULTILINE = ['ACME', 'Conf'];
 export const SITE_NAME = 'ACME Conf';
 export const META_DESCRIPTION =
   'This is an open source demo that Next.js developers can clone, deploy, and fully customize for events. Created through collaboration of marketers, designers, and developers at Vercel.';
 export const SITE_DESCRIPTION =
   'An interactive online experience by the community, free for everyone.';
-export const DATE = 'October 27, 2020';
-export const SHORT_DATE = 'Oct 27 - 9:00am PST';
-export const FULL_DATE = 'Oct 27th 9am Pacific Time (GMT-7)';
+export const DATE = 'Janeiro 05, 2022';
+export const SHORT_DATE = 'Jan 05 - 20:30pm PST';
+export const FULL_DATE = 'Jan 05th 6pm Pacific Time (GMT-7)';
 export const TWEET_TEXT = META_DESCRIPTION;
 export const COOKIE = 'user-id';
 
